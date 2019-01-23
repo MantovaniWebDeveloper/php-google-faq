e26c01a69de0af23696e26d68714e520cbcd69ae
MantovaniWebDeveloper <ratedsuperstar84@gmail.com>
2019-01-23 14:50:51 +0100

    inizio costruzione struttura base dati

--- index.php
+++ index.php
@@ -5,6 +5,17 @@
     <title>Faq privacy & terms</title>
   </head>
   <body>
+    <?php
+      /*BASE DATI*/
+      $database = [
+          "domande" => [
+            "prima" => "How are you implementing the recent Court of Justice of the European Union (CJEU) decision on the right to be forgotten?",
+            "seconda" => "How does Google protect my privacy and keep my information secure?",
+          ],
+        ] ;
 
+        var_dump($database);
+     ?>
+     <h2>esisto</h2>
   </body>
 </html>
