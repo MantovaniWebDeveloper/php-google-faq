88b0f19c11ef015ef88b4a36e826d05b7f6fc0c9
MantovaniWebDeveloper <ratedsuperstar84@gmail.com>
2019-01-23 15:23:05 +0100

    ora db funzionante e iniettato in una prima bozza di html

--- index.php
+++ index.php
@@ -8,33 +8,34 @@
     <?php
       /*BASE DATI*/
       $database = [
-          "domande" => [
-            "domanda" => "How are you implementing the recent Court of Justice of the European Union (CJEU) decision on the right to be forgotten?",
-          ],
-          [
-            "domanda" => "How does Google protect my privacy and keep my information secure?",
-          ],
-
-          "risposte" => [
-            "risposta" => "risposta 1",
-          ],
-          [
-            "risposta" => "risposta 2",
-          ],
-        ] ;
+          "faq" => [
+                    "domanda" => "How are you implementing the recent Court of Justice of the European Union (CJEU) decision on the right to be forgotten?",
+                    "risposta" => "risposta1",
+                  ],
+                  [
+                  "domanda" => "How does Google protect my privacy and keep my information secure?",
+                  "risposta" => "risposta2",
+                  ],
+                  [
+                  "domanda" => "How can I remove information about myself from Google's search results?",
+                  "risposta" => "risposta3",
+                  ],
+        ];
         echo '<pre>';
         var_dump($database);
         echo '</pre>';
 
         echo "<br />";
-
-        foreach ($database as $k => $dato) {
-          echo $dato["domanda"];
-          echo $dato["risposta"];
-          echo "<br />";
-
-        }
      ?>
-     <h2>esisto</h2>
+     <div class="container">
+        <?php foreach ($database as $k => $dato) { ?>
+         <div class="domanda">
+          <?php echo $dato["domanda"]; ?>
+         </div>
+         <div class="risposta">
+          <?php echo $dato["risposta"]; ?>
+         </div>
+       <?php } ?>
+     </div>
   </body>
 </html>
