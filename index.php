0e3078b64b13a2dc5fdd485edb59e46ef0fe8d1f
MantovaniWebDeveloper <ratedsuperstar84@gmail.com>
2019-01-23 15:07:19 +0100

    prima bozza db funzionante con stampa domanda e risposte

--- index.php
+++ index.php
@@ -9,12 +9,31 @@
       /*BASE DATI*/
       $database = [
           "domande" => [
-            "prima" => "How are you implementing the recent Court of Justice of the European Union (CJEU) decision on the right to be forgotten?",
-            "seconda" => "How does Google protect my privacy and keep my information secure?",
+            "domanda" => "How are you implementing the recent Court of Justice of the European Union (CJEU) decision on the right to be forgotten?",
+          ],
+          [
+            "domanda" => "How does Google protect my privacy and keep my information secure?",
+          ],
+
+          "risposte" => [
+            "risposta" => "risposta 1",
+          ],
+          [
+            "risposta" => "risposta 2",
           ],
         ] ;
-
+        echo '<pre>';
         var_dump($database);
+        echo '</pre>';
+
+        echo "<br />";
+
+        foreach ($database as $k => $dato) {
+          echo $dato["domanda"];
+          echo $dato["risposta"];
+          echo "<br />";
+
+        }
      ?>
      <h2>esisto</h2>
   </body>
