5f060c93c713392b7600156158b7a0a70b25164f
MantovaniWebDeveloper <ratedsuperstar84@gmail.com>
2019-01-23 15:25:32 +0100

    separato db da html, ora ho creato un file esterno che importo

---
+++ db.php
@@ -0,0 +1,26 @@
+<?php
+  /*BASE DATI*/
+  $database = [
+      "faq" => [
+                "domanda" => "How are you implementing the recent Court of Justice of the European Union (CJEU) decision on the right to be forgotten?",
+                "risposta" => "risposta1",
+              ],
+              [
+              "domanda" => "How does Google protect my privacy and keep my information secure?",
+              "risposta" => "risposta2",
+              ],
+              [
+              "domanda" => "How can I remove information about myself from Google's search results?",
+              "risposta" => "risposta3",
+              ],
+              [
+              "domanda" => "Are my search queries sent to websites when I click on Google Search results?",
+              "risposta" => "risposta3",
+              ],
+    ];
+  /*  echo '<pre>';
+    var_dump($database);
+    echo '</pre>';
+
+    echo "<br />"; */
+ ?>
