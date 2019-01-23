2b34b3ad9f25217de670da23aa1fbeb9fb48c475
MantovaniWebDeveloper <ratedsuperstar84@gmail.com>
2019-01-23 15:32:39 +0100

    prima suddivisione per header

--- index.php
+++ index.php
@@ -2,9 +2,20 @@
 <html lang="it" dir="ltr">
   <head>
     <meta charset="utf-8">
+    <link rel="stylesheet" href="css/style.css">
     <title>Faq privacy & terms</title>
   </head>
   <body>
+    <header>
+      <div id="topHeader">
+        <div class="wrapLogo">
+          <img src="https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_74x24dp.png" alt="">
+        </div>
+      </div>
+      <div id="subHeader">
+
+      </div>
+    </header>
     <?php include("db.php"); ?>
      <div class="container">
         <?php foreach ($database as $k => $dato) { ?>
