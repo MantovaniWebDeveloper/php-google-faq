94dc8372072b9b28cedcc38eb05f6da6deb292aa
MantovaniWebDeveloper <ratedsuperstar84@gmail.com>
2019-01-23 15:54:44 +0100

    prima parte sotto menu fatta del header

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
   <head>
     <meta charset="utf-8">
     <link rel="stylesheet" href="css/style.css">
+    <link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet">
     <title>Faq privacy & terms</title>
   </head>
   <body>
@@ -10,11 +11,22 @@
       <div class="container-large">
         <div id="topHeader">
           <div class="wrapLogo">
-            <img src="https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_74x24dp.png" alt="">
+            <img src="https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_74x24dp.png" alt="logo-google"/>
+          </div>
+          <div class="wrapLogoPrivacy">
+            <h2>Privacy & Terms</h2>
           </div>
         </div>
         <div id="subHeader">
-
+          <nav class="wrapNav">
+            <ul class="nav">
+              <li><a href="">Overview</a></li>
+              <li><a href="">Privacy Policy</a></li>
+              <li><a href="">Terms of Service</a></li>
+              <li><a href="">Technologies</a></li>
+              <li><a href="">FAQ</a></li>
+            </ul>
+          </nav>
         </div>
       </div>
     </header>
